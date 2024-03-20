fa71908657dab17079994a8b0af295d8b09aa31d
Aleksi Toivonen <aptoivonen@gmail.com>
2024-03-20 15:57:09 +0200

    chore: export primary color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 
+export const primary = "#1e384c";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -12,7 +14,7 @@ export default {
     colors: {
       transparent: "transparent",
       current: "currentColor",
-      primary: "#1e384c",
+      primary,
       secondary: "hsl(0, 0%, 89%)", // #e4e4e4
       "secondary-tinted": "hsl(0, 0%, 74%)",
       tertiary: "#ffa500",
